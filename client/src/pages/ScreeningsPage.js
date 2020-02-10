8755640dba1f50a7faecb6398c872058d6154e8f
Beniamin Abramowicz <beniamin.abramowicz@gmail.com>
2020-02-10 09:56:03 +0100

    Add Screenings page

---
+++ client/src/pages/ScreeningsPage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import apis from '../api/index'
+
+class ScreeningsPage extends React.Component{
+
+    state = {screenings: [] };
+
+    componentDidMount = async () => {
+        const response = await apis.getAllScreenings();
+        this.setState({screenings: response.data});
+    }
+
+    render(){
+        return (
+            <div>
+                <ScreeningsComponent screeningsList={this.state.screenings}/>
+            </div>
+        )
+    }
+}
+
+export default ScreeningsPage
